00627ef9125a68131c0cd68c7b9d2b099f788833
FremyXS <70861067+FremyXS@users.noreply.github.com>
2020-09-26 10:04:10 +0500

    3

--- lab01.cs
+++ lab01.cs
@@ -1,128 +1,109 @@
 ﻿using System;
+using System.Net.Http;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
-namespace lab01_ver2
+namespace Lav01_ver3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            DateTime dt = DateTime.Today;
+            string data1 = dt.ToString("d");
+            int datden = Convert.ToInt32(Convert.ToString(data1[0]) + Convert.ToString(data1[1]));
+            int datmes = Convert.ToInt32(Convert.ToString(data1[3]) + Convert.ToString(data1[4]));
+            int datgod = Convert.ToInt32(Convert.ToString(data1[6]) + Convert.ToString(data1[7]) + Convert.ToString(data1[8]) + Convert.ToString(data1[9]));
             Console.WriteLine("Привет. Введи своё Имя:");
             string name = Console.ReadLine();
-            Console.WriteLine($"Привет {name}. Теперь введи дату на сегодня:");
+            Console.WriteLine($"Привет {name}. Теперь введи дату своего дня рождения:");
             Console.WriteLine("Год");
-            int x = Convert.ToInt32(Console.ReadLine());
+            int god = 0;
+            ProvGod(datgod, ref god);
             Console.WriteLine("Месяц");
-            int y = Convert.ToInt32(Console.ReadLine());
-            if (y < 1 || y > 12) 
-            {
-                Console.WriteLine($"Нет такого месяца как {y}");
-                return;
-            }
+            int mes = 0;
+            ProvMes(ref mes);
             Console.WriteLine("День");
-            int z = Convert.ToInt32(Console.ReadLine());
-            if (y == 2) //если месяц февраль, то проверяем на високосный год
+            int den = 0;
+            ProvDnya(mes, god, ref den);
+            int vozrast = datgod - god;
+            if (mes > datmes) 
+                vozrast -= 1;
+            else if (mes == datmes)
             {
-                if (x % 4 == 0)
+                if (den > datden)
+                    vozrast -= 1;
+            }
+            Console.WriteLine($"Привет {name}. Ваш возраст равен {vozrast} лет");
+        }
+        static void ProvMes(ref int x) 
+        {
+            int mes;
+            do
+            {
+                mes = Convert.ToInt32(Console.ReadLine());
+                if (mes < 1 || mes > 12)
+                    Console.WriteLine("Нет такого месяца");
+                else
+                    break;
+            } while (mes < 1 || mes > 12);
+            x += mes;
+        }
+        static void ProvDnya(int mes, int god, ref int x) 
+        {
+            int den;
+            if (mes == 2)
+            {
+                if (god % 4 == 0)
                 {
-                    if (z < 1 || z > 29) 
+                    do
                     {
-                        Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z}");
-                        return;
-                    }
+                        den = Convert.ToInt32(Console.ReadLine());
+                        if (den < 1 || den > 29)
+                            Console.WriteLine($"Нет такого дня как {den}");
+                        else
+                            break;
+                    } while (den < 1 || den > 29);
                 }
                 else
                 {
-                    if (z < 1 || z > 28)
+                    do
                     {
-                        Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z}");
-                        return;
-                    }
+                        den = Convert.ToInt32(Console.ReadLine());
+                        if (den < 1 || den > 28)
+                            Console.WriteLine($"Нет такого дня как {den}");
+                        else
+                            break;
+                    } while (den < 1 || den > 28);
                 }
             }
-            else if (y == 1 | y == 3 | y == 5 | y == 7 | y == 8 | y == 10 | y == 12) //если у месяца всего 31 день, то выполняем проверку на дни
+            else if (mes == 1 | mes == 3 | mes == 5 | mes == 7 | mes == 8 | mes == 10 | mes == 12)
             {
-                if (z < 1 || z > 31)
-                {
-                    Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z}");
-                    return;
-                }
+                den = Convert.ToInt32(Console.ReadLine());
+                if (den < 1 || den > 31)
+                    Console.WriteLine($"Нет такого дня как {den}");
             }
-            else if (y == 4 | y == 6 | y == 9 | y == 11) //если у месяца всего 30 дней, то выполняем проверку
+            else
             {
-                if (z < 1 || z > 30) 
-                {
-                    Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z}");
-                    return;
-                }
+                den = Convert.ToInt32(Console.ReadLine());
+                if (den < 1 || den > 30)
+                    Console.WriteLine($"Нет такого дня как {den}");
             }
-
-            Console.WriteLine("Молодец!!!");
-            Console.WriteLine("Теперь введи дату своего дня рождения:");
-            Console.WriteLine("Год");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            if (x < x2)
+            x += den;
+        }
+        static void ProvGod(int datgod, ref int x) 
+        {
+            int god;
+            do
             {
-                Console.WriteLine("Ошибка. Год Дня рождения превышает год, который идёт сегодня");
-                return;
-            }
-            Console.WriteLine("Месяц");
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            if (y2 < 1 || y2 > 12)
-            {
-                Console.WriteLine($"Нет такого месяца как {y2}");
-                return;
-            }
-            Console.WriteLine("День");
-            int z2 = Convert.ToInt32(Console.ReadLine());
-            if (y2 == 2) //если месяц февраль, то проверяем на високосный год
-            {
-                if (x2 % 4 == 0)
-                {
-                    if (z2 < 1 || z2 > 29)
-                    {
-                        Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z2}");
-                        return;
-                    }
-                }
+                god = Convert.ToInt32(Console.ReadLine());
+                if (god > datgod)
+                    Console.WriteLine("Не верно. Ваш год дня рождения превышает больше сегоднешнего года.");
                 else
-                {
-                    if (z2 < 1 || z2 > 28)
-                    {
-                        Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z2}");
-                        return;
-                    }
-                }
-            }
-            else if (y2 == 1 | y2 == 3 | y2 == 5 | y2 == 7 | y2 == 8 | y2 == 10 | y2 == 12) //если у месяца всего 31 день, то выполняем проверку на дни
-            {
-                if (z2 < 1 || z2 > 31)
-                {
-                    Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z2}");
-                    return;
-                }
-            }
-            else if (y2 == 4 | y2 == 6 | y2 == 9 | y2 == 11) //если у месяца всего 30 дней, то выполняем проверку
-            {
-                if (z2 < 1 || z2 > 30)
-                {
-                    Console.WriteLine($"Уууупс... В этом месяце нет такого дня как {z2}");
-                    return;
-                }
-            }            
-
-            int razg = x - x2;
-            if (y2 > y) 
-            {
-                razg -= 1;
-            }
-            else if(y2 == y) 
-            {
-                if (z2 > z) 
-                {
-                    razg -= 1;
-                }   
-            }
-            Console.WriteLine($"Привет {name}, твой возраст равен {razg} лет");
-        }  
+                    break;
+            } while (god > datgod);
+            x += god;
+        }
     }
 }
